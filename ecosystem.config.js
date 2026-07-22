43d6b82105852b1b53b97ffd45fe996cd6728e97
parthpbhagat <TAMARO_REAL_EMAIL@gmail.com>
2026-07-22 12:22:55 +0530

    feat: add epfo-scraper app to PM2 ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,5 +13,16 @@ module.exports = {
         PYTHONUNBUFFERED: "1",
       },
     },
+    {
+      name: "epfo-scraper",
+      script: "epfo_scraper.py",
+      interpreter: "python",
+      args: "--company-file all_companies.txt --no-search-variants --skip-existing-statuses completed,searched,skipped --delay 1.5 --details-workers 1",
+      cwd: __dirname,
+      autorestart: false,
+      env: {
+        PYTHONUNBUFFERED: "1",
+      },
+    },
   ],
 };
